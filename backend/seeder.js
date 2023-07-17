1a800941bbd21d3a6c71f939e374783780b344df
sholabioku <bilushonline@gmail.com>
2023-07-17 09:51:45 +0100

    Add functionality to destroy data

--- backend/seeder.js
+++ backend/seeder.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import colors from 'colors';
 
-import users from './data/users';
-import products from './data/products';
+import users from './data/users.js';
+import products from './data/products.js';
 import User from './models/userModel.js';
 import Product from './models/productModel.js';
 import Order from './models/orderModel.js';
@@ -11,7 +11,7 @@ import connectDB from './config/db.js';
 
 dotenv.config();
 
-connectBD();
+connectDB();
 
 const importData = async () => {
   try {
@@ -36,3 +36,23 @@ const importData = async () => {
     process.exit(1);
   }
 };
+
+const destroyData = async () => {
+  try {
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    console.log('Data Destroyed!'.red.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`.red.inverse);
+    process.exit(1);
+  }
+};
+
+if (process.argv[2] === '-d') {
+  destroyData();
+} else {
+  importData();
+}
