e569a39c77783b3516b4b43d7e8d9375df832ee3
sholabioku <bilushonline@gmail.com>
2023-07-19 14:20:33 +0100

    Add functionality to create productRoute

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,9 @@ import colors from 'colors';
 import dotenv from 'dotenv';
 dotenv.config();
 
-import products from './data/products.js';
 import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+
 const port = process.env.PORT || 5000;
 
 connectDB(); //Connect to MongoDB
@@ -15,14 +16,7 @@ app.get('/', (req, res) => {
   res.send('API is running....');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 app.listen(port, () =>
   console.log(`Server running on port ${port}`.yellow.bold)
