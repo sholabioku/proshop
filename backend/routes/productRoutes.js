ddfc5d563a83af23c4c3b6513904f9900568b170
sholabioku <bilushonline@gmail.com>
2023-07-19 14:52:12 +0100

    Add functionality to fetch all and single products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 const router = express.Router();
 
-import products from '../data/products.js';
 import asyncHandler from '../middleware/asyncHandler.js';
+import Product from '../models/productModel.js';
 
 router.get(
   '/',
   asyncHandler(async (req, res) => {
+    const products = await Product.find({});
     res.json(products);
   })
 );
@@ -14,8 +15,13 @@ router.get(
 router.get(
   '/:id',
   asyncHandler(async (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      return res.json(product);
+    }
+
+    res.status(404).json({ message: 'Product not found' });
   })
 );
 
