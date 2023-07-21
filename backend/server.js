0041142923130115534eca28b801adf063843297
sholabioku <bilushonline@gmail.com>
2023-07-21 21:14:05 +0100

    Add functionality for errorMiddleware

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5000;
 
@@ -17,6 +18,9 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () =>
   console.log(`Server running on port ${port}`.yellow.bold)
