e2adfcb2a8dbd5203da7206d6e9311afa0973157
sholabioku <bilushonline@gmail.com>
2023-07-23 07:19:43 +0100

    Add functionality to create cart slice

---
+++ frontend/src/slices/cartSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = localStorage.getItem('cart')
+  ? JSON.parse(localStorage.getItem('cart'))
+  : { cartItems: [] };
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {},
+});
+
+export const {} = cartSlice.actions;
+
+export default cartSlice.reducer;
