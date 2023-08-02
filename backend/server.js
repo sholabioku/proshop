368d20ea49e0801d66537b893673db6147584f0e
sholabioku <bilushonline@gmail.com>
2023-08-02 10:38:39 +0100

    Add functionality to authenticate user

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,9 @@ connectDB(); //Connect to MongoDB
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.send('API is running....');
 });
