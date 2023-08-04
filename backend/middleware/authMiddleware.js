f04ab11d5dda35b40f994eeebf986e7e6eebed3b
sholabioku <bilushonline@gmail.com>
2023-08-04 17:25:04 +0100

    Add functionality to create admin middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -22,3 +22,12 @@ export const protect = asyncHandler(async (req, res, next) => {
     throw new Error('Not authorized, no token');
   }
 });
+
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error('Not authorized as admin');
+  }
+};
