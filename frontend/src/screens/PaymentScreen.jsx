0ebe48e0af4afef1416faffaac7149f33fe0c6f8
sholabioku <bilushonline@gmail.com>
2023-10-12 07:29:52 +0100

    Add functionality for PaymentScreen complete

--- frontend/src/screens/PaymentScreen.jsx
+++ frontend/src/screens/PaymentScreen.jsx
@@ -1,17 +1,39 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Form, Button, Col } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 import FormContainer from '../components/FormContainer';
 import CheckoutSteps from '../components/CheckoutSteps';
+import { savePaymentMethod } from '../slices/cartSlice';
 
 const PaymentScreen = () => {
   const [paymentMethod, setPaymentMethod] = useState('PayPal');
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const cart = useSelector((state) => state.cart);
+
+  const { shippingAddress } = cart;
+
+  useEffect(() => {
+    if (!shippingAddress) {
+      navigate('/shipping');
+    }
+  }, [shippingAddress, navigate]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(savePaymentMethod(paymentMethod));
+    navigate('/placeorder');
+  };
 
   return (
     <FormContainer>
       <CheckoutSteps step1 step2 step3 />
       <h1>Payment Method</h1>
-      <Form>
+      <Form onSubmit={submitHandler}>
         <Form.Group>
           <Form.Label as='legend'>Select Method</Form.Label>
           <Col>
