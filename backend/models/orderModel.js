7bf8696170f92612e0ea5ed9a2bd3a92ba479914
sholabioku <bilushonline@gmail.com>
2024-01-31 14:57:26 +0100

    Add functionality for POST /api/orders GET /api/orders/myorders GET /api/orders/:id routes

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -41,6 +41,11 @@ const orderSchema = new mongoose.Schema(
       required: true,
       default: 0.0,
     },
+    taxPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
     shippingPrice: {
       type: Number,
       required: true,
