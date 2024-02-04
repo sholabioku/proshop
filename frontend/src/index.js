10b0b7bc3de8d9ed4f16b391a3715c1544c574ad
sholabioku <bilushonline@gmail.com>
2024-02-04 09:45:38 +0100

    Add functionality for PUT /api/orders/:id/pay query

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,7 @@ import {
   RouterProvider,
 } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
@@ -47,7 +48,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <PayPalScriptProvider deferLoading={true}>
+        <RouterProvider router={router} />
+      </PayPalScriptProvider>
     </Provider>
   </React.StrictMode>
 );
