39d060e0316d62f036c572030ed5b551afc66b15
sholabioku <bilushonline@gmail.com>
2024-02-08 17:20:11 +0100

    Add functionality for POST /api/products route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,11 +2,14 @@ import express from 'express';
 const router = express.Router();
 
 import {
+  createProduct,
   getProductById,
   getProducts,
 } from '../controllers/productController.js';
 
-router.route('/').get(getProducts);
+import { protect, admin } from '../middleware/authMiddleware.js';
+
+router.route('/').get(getProducts).post(protect, admin, createProduct);
 router.route('/:id').get(getProductById);
 
 export default router;
