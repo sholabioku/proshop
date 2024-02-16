e2d797e32b379aeddc6e17f45f9825ef3024a4dc
sholabioku <bilushonline@gmail.com>
2024-02-16 17:33:50 +0100

    Add functionality to submit created review

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -88,10 +88,12 @@ const createProductReview = asyncHandler(async (req, res) => {
   const { rating, comment } = req.body;
 
   const product = await Product.findById(req.params.id);
+
   if (product) {
     const alreadyReviewed = product.reviews.find(
-      (review) => review.user.toString() === req.user._id.toString()
+      (r) => r.user.toString() === req.user._id.toString()
     );
+
     if (alreadyReviewed) {
       res.status(400);
       throw new Error('Product already reviewed');
@@ -105,17 +107,18 @@ const createProductReview = asyncHandler(async (req, res) => {
     };
 
     product.reviews.push(review);
+
     product.numReviews = product.reviews.length;
+
     product.rating =
-      product.reviews.reduce((acc, review) => {
-        acc + review.rating;
-      }, 0) / product.reviews.length;
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length;
 
     await product.save();
     res.status(201).json({ message: 'Review added' });
   } else {
     res.status(404);
-    throw new Error('Resource not found');
+    throw new Error('Product not found');
   }
 });
 
